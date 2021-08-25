e14161668d0376baacec7336eed5d6d2ff976d1e
Neon <58448036+NeonGamerBot-QK@users.noreply.github.com>
2021-08-24 21:23:40 -0400

    Update test.js

--- src/test.js
+++ src/test.js
@@ -1,5 +1,5 @@
 let {Wrapper} = require('./index');
-let test = new Wrapper({ key: 'NzY1NTc4NTI1ODE4MDkzNjA4.X4W2kA.MrGISUHfmkPbqnFsGVixO9MwbBU', })
+let test = new Wrapper({ key: process.env.key, })
 
 test.randomtext(10).then(console.log);
 
